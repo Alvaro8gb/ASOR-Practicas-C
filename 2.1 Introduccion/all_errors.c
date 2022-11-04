b4720b07aa1b098abbd92d3eb30ba8b22f330b21
Álvaro Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2022-11-04 16:32:01 +0100

    first commit
    Iniciamos practicas

---
+++ 2.1 Introduccion/all_errors.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+int main(){
+ 
+
+   for(int i = 1; i<255; i++){
+         printf("Codigo de error :%d \t Mensaje asociado: %s\n",i, strerror(i));
+   }
+
+    return 0;
+}
---
+++ 2.1 Introduccion/control_errors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <errno.h>
+
+
+
+int main(){
+ 
+
+    if ( setuid(0) == -1){
+        perror(NULL);
+        printf("Codigo de error :%d \t Mensaje asociado: %s\n",errno, strerror(errno));
+    }
+
+
+    return 1;
+}
